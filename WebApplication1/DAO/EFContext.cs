7bdbb3ed178fb6a24138f839f619fdbcdacbc3b0
Leandro Santos <leandrosancosta@gmail.com>
2021-02-04 22:41:52 -0300

    Start

---
+++ WebApplication1/DAO/EFContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAO
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("EFConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Universidade> Universidades { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+
+    }
+}
---
+++ learning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
